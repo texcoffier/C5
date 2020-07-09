c5ed79d2cf3647bdce4e2c5d585696052079e8a9
Thierry EXCOFFIER <thierry.excoffier@univ-lyon1.fr>
2020-07-09 22:28:44 +0200

    Clear pylint warnings

--- ccccc.py
+++ ccccc.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+# pylint: disable=invalid-name,too-many-arguments,too-many-instance-attributes
 
 """
 To simplify the class contains the code for the GUI and the worker.
@@ -10,22 +11,45 @@ CCCCC_JS_1  is a subclass defining an exercice
 TODO : tester function
 
 """
+
+
 try:
-    window
-    in_browser = True
+    # pylint: disable=undefined-variable,missing-docstring,too-few-public-methods
+    print(str("Python"))
+    in_python = True
+    window = window
     document = document
-    postMessage =  postMessage
-    Error = Error
+    postMessage = postMessage
+    Error = None
     @external
     class Worker:
-        pass
-except:
+        def postMessage(self, _message):
+            pass
+    def bind(fct, obj):
+        """Bind the function to the object"""
+        return fct.bind(obj)
+except: # pylint: disable=bare-except,redefined-builtin
+    in_python = False
+    def str(txt):
+        """Python like"""
+        return txt.toString()
+    def bind(fct, _obj):
+        """Bind the function to the object: nothing to do in Python"""
+        return fct
+
+try:
+    print(window.location)
+    in_browser = True
+except: # pylint: disable=bare-except
     in_browser = False
 
 def html(txt):
-    return str(txt).replace("&", "&amp;").replace("<","&lt;").replace(">","&gt;")
+    """Protect text to display it in HTML"""
+    return str(txt).replace("&", "&amp;").replace("<", "&lt;").replace(">", "&gt;")
+
 
 def new_element(htmltype, htmlclass, left, width, top, height, background):
+    """Create a DOM element"""
     e = document.createElement(htmltype)
     e.className = htmlclass
     e.style.position = 'absolute'
@@ -45,17 +69,19 @@ class CCCCC:
     compiler_height = 30
     question = editor = tester = compiler = executor = None # HTML elements
     executable = None # Compilation result (worker side)
+    top = None # Top page HTML element
 
     def __init__(self):
         self.worker = None
         if in_browser:
             self.worker = Worker('xxx-ccccc.js')
-            self.worker.onmessage      = self.onmessage.bind(self)
-            self.worker.onmessageerror = self.onmessage.bind(self)
-            self.worker.onerror        = self.onmessage.bind(self)
+            self.worker.onmessage = bind(self.onmessage, self)
+            self.worker.onmessageerror = bind(self.onmessage, self)
+            self.worker.onerror = bind(self.onmessage, self)
             print(self.worker)
 
     def create_question(self):
+        """The question text container: course and exercise"""
         e = new_element('DIV', 'question',
                         0, self.question_width,
                         0, self.question_height,
@@ -65,7 +91,8 @@ class CCCCC:
         self.top.appendChild(e)
 
     def create_tester(self):
-        e = new_element('DIV', 'question',
+        """The regression test container"""
+        e = new_element('DIV', 'regtests',
                         0, self.question_width,
                         self.question_height, 100 - self.question_height,
                         '#EFE')
@@ -73,6 +100,7 @@ class CCCCC:
         self.top.appendChild(e)
 
     def create_editor(self):
+        """The text editor container"""
         e = new_element('DIV', 'editor',
                         self.question_width, self.source_width,
                         0, 100,
@@ -83,6 +111,7 @@ class CCCCC:
         self.editor.focus()
 
     def create_compiler(self):
+        """The compiler result container"""
         e = new_element('DIV', 'compiler',
                         self.question_width + self.source_width,
                         100 - self.question_width - self.source_width,
@@ -92,6 +121,7 @@ class CCCCC:
         self.top.appendChild(e)
 
     def create_executor(self):
+        """The execution result container"""
         e = new_element('DIV', 'executor',
                         self.question_width + self.source_width,
                         100 - self.question_width - self.source_width,
@@ -103,19 +133,22 @@ class CCCCC:
     def run(self, source):
         """This method runs in the worker"""
         self.executable = self.run_compiler(source)
+        postMessage(['run', None])
         if self.executable:
-            postMessage(['run', None])
             self.run_executor([])
     def onmousedown(self, event):
-        print("mouse down")
+        """Mouse down"""
         self.editor.focus()
         event.preventDefault(True)
-    def onkeydown(self, event):
+    def onkeydown(self, event): # pylint: disable=no-self-use
+        """Key down"""
         if event.key == 'Tab':
             event.preventDefault(True)
     def onkeyup(self, _event):
+        """Key up"""
         self.worker.postMessage(self.editor.textContent)
     def onkeypress(self, event):
+        """Key press"""
         pass
     def onmessage(self, event):
         """Interprete messages from the worker"""
@@ -132,11 +165,12 @@ class CCCCC:
             e.innerHTML += event.data[1]
 
     def create_html(self):
-        self.top             = document.createElement('DIV')
-        self.top.onmousedown = self.onmousedown.bind(self)
-        self.top.onkeydown   = self.onkeydown.bind(self)
-        self.top.onkeyup     = self.onkeyup.bind(self)
-        self.top.onkeypress  = self.onkeypress.bind(self)
+        """Create the page content"""
+        self.top = document.createElement('DIV')
+        self.top.onmousedown = bind(self.onmousedown, self)
+        self.top.onkeydown = bind(self.onkeydown, self)
+        self.top.onkeyup = bind(self.onkeyup, self)
+        self.top.onkeypress = bind(self.onkeypress, self)
         document.getElementsByTagName('BODY')[0].appendChild(self.top)
         self.create_question()
         self.create_tester()
@@ -144,21 +178,28 @@ class CCCCC:
         self.create_compiler()
         self.create_executor()
 
-    def compiler_initial_content(self):
+    def compiler_initial_content(self): # pylint: disable=no-self-use
+        """Used by the subclass"""
         return "RESULTAT DE LA COMPILATION<hr>"
-    def executor_initial_content(self):
+    def executor_initial_content(self): # pylint: disable=no-self-use
+        """Used by the subclass"""
         return "RESULTAT DE L'EXÉCUTION<hr>"
-    def question_initial_content(self):
+    def question_initial_content(self): # pylint: disable=no-self-use
+        """Used by the subclass"""
         return "Please redefined this function"
-    def run_compiler(self, _source):
+    def run_compiler(self, _source): # pylint: disable=no-self-use
+        """Do the compilation"""
         postMessage(['compile', 'No compiler defined'])
-    def run_executor(self, _args):
-        postMessage(['compile', 'No executor defined'])
+    def run_executor(self, _args): # pylint: disable=no-self-use
+        """Do the execution"""
+        postMessage(['run', 'No executor defined'])
 
 class CCCCC_JS(CCCCC):
+    """JavaScript compiler and evaluator"""
     def run_compiler(self, source):
         postMessage(['compile', None])
         try:
+            # pylint: disable=eval-used
             f = eval('''function _tmp_(args)
             {
                function print(txt)
@@ -177,7 +218,6 @@ class CCCCC_JS(CCCCC):
                          '<error>'
                          + html(err.name) + '<br>\n' + html(err.message)
                          + '</error>'])
-            postMessage(['run', None])
     def run_executor(self, args):
         try:
             self.executable(args)
@@ -187,6 +227,7 @@ class CCCCC_JS(CCCCC):
                          + html(err.message) + '</error>'])
 
 class CCCCC_JS_1(CCCCC_JS):
+    """First exercise"""
     def question_initial_content(self):
         return """Pour afficher quelque chose, on tape :
 <pre>
@@ -203,6 +244,7 @@ if in_browser:
     ccccc.create_html()
 else:
     def onmessage(event):
+        """Evaluate immediatly the function if in the worker"""
         ccccc.run(event.data.toString())
 
 print("ok")
