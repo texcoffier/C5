477065b8a4507cf3a539ab37b5b1e6b9a966e5b0
Thierry EXCOFFIER <thierry.excoffier@univ-lyon1.fr>
2022-06-21 16:58:43 +0200

    fixup! Documentation update

---
+++ course_js_example.py
@@ -0,0 +1,33 @@
+# pylint: disable=no-self-use,missing-function-docstring
+"""
+Demonstration of the system
+"""
+
+class Q0(Question): # pylint: disable=undefined-variable
+    """Question 0"""
+    def question(self):
+        return """La réponse est : «print(1)»"""
+    def tester(self):
+        self.display(
+            "Pour le moment la zone en bas à droite contient :<pre>"
+            + self.worker.escape(self.worker.execution_result) + """</pre>""")
+        self.check(self.worker.execution_result,
+                   [
+                       ['^ *1 *\n$', "Contient 1"],
+                   ])
+        if self.all_tests_are_fine:
+            self.next_question()
+            return
+    def default_answer(self):
+        return """print(2);"""
+
+class QEnd(Question): # pylint: disable=undefined-variable
+    """Question Finale"""
+    def question(self):
+        return "Plus de questions"
+    def tester(self):
+        self.display('FINI !')
+    def default_answer(self):
+        return ""
+
+Compile_JS([Q0(), QEnd()]) # pylint: disable=undefined-variable
