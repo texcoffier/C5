21c0d2dcec1efe628ce166c7f392891bdf5fb362
Thierry EXCOFFIER <thierry.excoffier@univ-lyon1.fr>
2022-07-08 17:59:33 +0200

    Adm course page : nicer statistics

--- adm_course.py
+++ adm_course.py
@@ -6,6 +6,9 @@ window = window
 isNaN = isNaN
 Math = Math
 
+MAX_WIDTH = 600
+SNAIL = 4
+
 def analyse(http_server): # pylint: disable=too-many-locals,too-many-branches,too-many-statements
     """Extract statistiques from log"""
     answered = []
@@ -80,9 +83,9 @@ WHAT = ['nr_answered', 'key_stroke', 'mouse_click',
         'copy_ok', 'copy_bad', 'paste_ok', 'paste_bad'
        ]
 
-COLORS = ["444", "F00", "FF0", "0F0", "0FF", "00F", "F0F", "CCC"]
+COLORS = ["888", "F44", "FF0", "0F0", "0FF", "88F", "F0F", "CCC"]
 
-def display():
+def display(): # pylint: disable=too-many-locals
     """Create the admin home page"""
     document.title = "C5 " + COURSE
     students = []
@@ -98,26 +101,41 @@ TABLE { border-spacing: 0px; border-collapse: collapse }
 TABLE TD { vertical-align: top; border: 1px solid #888; padding: 0px}
 TEXTAREA { border: 0px; min-height:10em; font-size:60%; width:10em; height:100% }
 BUTTON { width: 100% }
+E { font-family: emoji }
 """]
     for i, color in enumerate(COLORS):
         text.append(
-            '.sec' + i + '{ display: inline-block; height: 1em; vertical-align: top;'
-            + 'height: 1.3em; background: #' + color + '}')
+            'SPAN.sec' + i + '{ display: inline-block; height: 1em; vertical-align: top;'
+            + 'height: 1.3em;}')
+        text.append('.sec' + i + '{ background: #' + color + '}')
     text.append("""
 </style>
+<p>
 <table border>
     <tr><th>Login<th colspan="2">Questions<br>Validated<th>Keys
-        <th>Mouse<th>Copy<th>Copy<br>Fail<th>Paste<th>Paste<br>Fail<th>Time per question<th>Files</tr>
+        <th>Mouse<th>Copy<th>Copy<br>Fail<th>Paste<th>Paste<br>Fail<th>Time per question<br>
+        <E>🐌</E> : clipped to """ + SNAIL + """ times the median answer time.<th>Files</tr>
 """)
     cache = {}
-    max_time = 600
     question_times = []
     for login in students:
         student = STUDENTS[login]
         cache[login] = analyse(student.http_server)
-        full_time = sum(cache[login]['time_sum'])
-        if full_time > max_time:
-            max_time = full_time
+        for i, seconds in enumerate(cache[login]['time_sum']):
+            if not question_times[i]:
+                question_times[i] = []
+            question_times[i].append(seconds)
+    question_medians = []
+    for i, times in enumerate(question_times):
+        times.sort()
+        middle = Math.floor(len(times) // 2)
+        if len(times) % 2 == 1:
+            median = times[middle]
+        else:
+            median = (times[middle-1] + times[middle]) / 2
+        question_medians[i] = median
+    max_time = Math.min(MAX_WIDTH, sum(question_medians))
+
     for login in students:
         student = STUDENTS[login]
         stats = cache[login]
@@ -132,11 +150,13 @@ BUTTON { width: 100% }
 
         text.append('<td>')
         for i, seconds in enumerate(stats['time_sum']):
-            text.append('<span style="width:' + (600*seconds)/max_time
-                        + 'px" class="sec' + i % len(COLORS) + '"></span>')
-            if not question_times[i]:
-                question_times[i] = []
-            question_times[i].append(seconds)
+            if seconds > question_medians[i] * SNAIL:
+                seconds = question_medians[i] * SNAIL
+                more = '<E>🐌</E>'
+            else:
+                more = ''
+            text.append('<span style="width:' + (MAX_WIDTH*seconds)/max_time
+                        + 'px" class="sec' + i % len(COLORS) + '">' + more + '</span>')
         text.append('<td>')
         for filename in student.files:
             text.append(' <a target="_blank" href="adm_get/' + COURSE + '/')
@@ -156,13 +176,13 @@ BUTTON { width: 100% }
             <button onclick="window.location.pathname = 'adm_answers/' + COURSE + '.zip'"
             >Sources<br>txt ZIP</button>''')
     text.append('<td><table style="font-size: 90%">')
-    text.append('<tr><th>Question<th>Min<th>Average<th>Median<th>Max</tr>')
+    text.append('<tr><th>Question<th>Students<th>Min<th>Average<th>Median<th>Max</tr>')
     for i, times in enumerate(question_times):
-        times.sort()
-        text.append('<tr><td>' + (i+1)
+        text.append('<tr><td class="sec' + (i % len(COLORS)) + '">' + (i+1)
+                    + '<td>' + len(times)
                     + '<td>' + min(times)
                     + '<td>' + Math.floor(sum(times)/len(times))
-                    + '<td>' + times[Math.floor(len(times)/2)]
+                    + '<td>' + question_medians[i]
                     + '<td>' + max(times)
                     + '</tr>')
     text.append('</table></tr>')
