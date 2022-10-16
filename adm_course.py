034618cd0318d1f37fc62a3d0f85092372749c12
Thierry EXCOFFIER <thierry.excoffier@univ-lyon1.fr>
2022-10-16 10:59:33 +0200

    Adm course: display graders in a column

--- adm_course.py
+++ adm_course.py
@@ -102,7 +102,7 @@ def analyse(http_server, debug): # pylint: disable=too-many-locals,too-many-bran
             'time_bonus': time_bonus,
            }
 
-WHAT = ['nr_answered', 'grades', 'time', 'key_stroke', 'mouse_click',
+WHAT = ['nr_answered', 'grades', 'graders', 'time', 'key_stroke', 'mouse_click',
         'copy_ok', 'copy_bad', 'cut_ok', 'cut_bad', 'paste_ok', 'paste_bad', 'nr_blurs'
        ]
 
@@ -136,7 +136,7 @@ TABLE TR:hover TD { background: #EEE }
 </style>
 <p>
 <table border>
-    <tr><th>Login<th>Time<br>Bonus<th>Status<th colspan="2">Questions<br>Validated<th>Grade<th>Time<br>in sec.<th>Keys
+    <tr><th>Login<th>Time<br>Bonus<th>Status<th colspan="2">Questions<br>Validated<th>Grade<th>Graders<th>Time<br>in sec.<th>Keys
         <th>Mouse<th>Copy<th>Copy<br>Fail<th>Cut<th>Cut<br>Fail<th>Paste<th>Paste<br>Fail<th>Window<br>Blur<th>Time per question<br>
         <E>🐌</E> : clipped to """ + SNAIL + """ times the median answer time.<th>Files</tr>
 """)
@@ -148,9 +148,16 @@ TABLE TR:hover TD { background: #EEE }
         cache[login] = analyse(student.http_server, login == 'p2202699')
         grading = parse_grading(student['grades'])
         grade = 0
+        graders = []
         for question in grading:
             grade += Number(grading[question][0])
+            if len(grading[question][0]):
+                grader = grading[question][1].split('\n')[1]
+                if grader not in graders:
+                    graders.append(grader)
         cache[login]['grades'] = grade
+        graders.sort()
+        cache[login]['graders'] = ' '.join(graders)
         for i, seconds in enumerate(cache[login]['time_sum']):
             if not question_times[i]:
                 question_times[i] = []
