983424669f26de647408eaf6452538b217782f4f
Thierry EXCOFFIER <thierry.excoffier@univ-lyon1.fr>
2026-04-29 09:35:37 +0200

    Editor HTML: add indent

--- compile_html.py
+++ compile_html.py
@@ -9,7 +9,39 @@ class Session(Compile): # pylint: disable=undefined-variable,invalid-name
     def run_compiler(self, source):
         """Nothing to do"""
         self.post('compiler', 'ok')
-        return source
+        return '<div style="white-space:initial;font-family:sans-serif">' + source + '</div>'
     def run_executor(self):
         """Execute the compiled code"""
         self.post('executor', self.executable)
+    def run_indent(self, source):
+        """Indent the source"""
+        need_space = 0
+        indenting = True
+        depth = 0
+        txt = []
+        for i, char in enumerate(source):
+            if indenting:
+                if char == ' ':
+                    if need_space == 0:
+                        continue # Remove space
+                    need_space -= 1
+                else:
+                    while need_space:
+                        txt.append(' ')
+                        need_space -= 1
+                    indenting = False
+            if char != ' ':
+                indenting = False
+                if char == '<':
+                    if source[i+1] == '/':
+                        depth -= 2
+                        if depth < 0:
+                            depth = 0
+                    else:
+                        if source[i+1:i+4].upper() not in ('IMG', 'BR>', 'BR/', 'HR>', 'HR/', 'INP'):
+                            depth += 2
+            txt.append(char)
+            if char == '\n':
+                indenting = True
+                need_space = depth
+        self.post('editor', ''.join(txt))
